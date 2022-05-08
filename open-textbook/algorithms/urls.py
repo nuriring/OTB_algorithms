4fe820c2698f62a0e217ecc31d8da9ce3902d403
오하민 <ohm55545@naver.com>
2022-05-08 23:45:57 +0900

    0508

--- open-textbook/algorithms/urls.py
+++ open-textbook/algorithms/urls.py
@@ -4,4 +4,7 @@ app_name = 'algorithms'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<int:problem_pk>/', views.detail, name='detail'),
+    path('<int:problem_pk>/update/', views.problem_update, name='problem_update'),
 ]
--- open-textbook/algorithms/views.py
+++ open-textbook/algorithms/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from algorithms.models import Problem
+from .forms import ProblemForm
 
 # Create your views here.
 
@@ -10,3 +11,39 @@ def index(request):
         'problems' : problems,
     }
     return render(request, 'algorithms/index.html', context)
+
+
+def create(request):
+    # if request.method == 'POST':
+        form = ProblemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('algorithms:index')
+        else:
+            form = ProblemForm()
+        context = {
+            'form':form,
+        }
+        return render(request, 'algorithms/create.html', context)
+
+def detail(request, problem_pk):
+    problem = get_object_or_404(Problem, pk=problem_pk)
+    context = {
+        'problem': problem,
+    }
+    return render(request, 'algorithms/detail.html', context)
+
+def problem_update(request, problem_pk):
+    problem = get_object_or_404(Problem, pk=problem_pk)
+    # if request.method == 'POST':
+    form = ProblemForm(request.POST, instance=problem)
+    if form.is_valid():
+        form.save()
+        return redirect('algorithms:detail', problem_pk)
+    else:
+        form = ProblemForm(instance=problem)
+    context = {
+        'problem': problem,
+        'form': form,
+    }
+    return render(request, 'algorithms/update.html', context)
