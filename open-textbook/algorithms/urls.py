45b98470e4ce9c61cda92135c9b7b5810f2a6375
오하민 <ohm55545@naver.com>
2022-05-14 02:08:01 +0900

    sidebar,pagenation

--- open-textbook/algorithms/urls.py
+++ open-textbook/algorithms/urls.py
@@ -5,7 +5,6 @@ app_name = 'algorithms'
 urlpatterns = [
     path('', views.problem_index, name='problem_index'),
     path('create/', views.problem_create, name='problem_create'),
-    path('search/', views.problem_search, name='problem_search'),
     path('<int:problem_pk>/', views.problem_detail, name='problem_detail'),
     path('<int:problem_pk>/update/', views.problem_update, name='problem_update'),
     path('<int:problem_pk>/delete/', views.problem_delete, name='problem_delete'),
